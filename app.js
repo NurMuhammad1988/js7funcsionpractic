23bf9e7e80178154d609375f555a22458d70d2d1
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-02-17 00:33:55 +0500

    jshsjkfsd

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+
+// function myfunction(a,b){
+
+//     // document.write('salom')
+
+//     return a + b;
+// }
+
+// document.write(myfunction(2,3));
+
+// var num = myfunction(2,3);
+// document.write(num);
+
+
+// var num = +prompt('raqam yozing');
+// var power = +prompt('darajasini yozing');
+
+// function func(qwe, zxc) {
+
+//     var res = 1;
+
+//     for (var i = 1; i <= zxc; i++) {
+
+//         res = res * qwe;
+//     }
+
+//     return res;
+
+// }
+
+// var resul = func(num, power);
+// document.write(`${num}  darajasi  ${power} = ${resul}`);
+
+
+// function print() {
+//     var msg = prompt('yozish kerak');
+//     var couint = prompt('nechta chiqarishi kerak');
+
+//     for (var  i = 0; i < couint; i++){
+//         document.write('<p>' + msg + '<hr>');
+//     }
+
+
+// }
+
+// print();
+
+
+
